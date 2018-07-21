56057f80c46ba3e41c41dd4e0ba303afee239e0c
Angie Kassab <anasab12@gmail.com>
2018-07-21 21:51:49 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -121,7 +121,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
         
-        programmer_hash[:alan_kay][:known_for] = "GUI"
+        programmer_hash[:dennis_ritchie][:known_for] = "GUI"
         return programmer_hash
      }
 
