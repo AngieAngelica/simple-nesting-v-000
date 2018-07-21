71b0b737b6029b7a030c2f11f4e71d1df67f5cd4
Angie Kassab <anasab12@gmail.com>
2018-07-21 21:21:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-  programmer_hash[:grace_hopper]
+     programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         }
      }
      
-     
+     programmer_hash[:grace_hopper]
 end
 
 def dennis_ritchies_language
